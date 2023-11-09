d37c59890582d2aa1aca0bda2bee74e282f0b5ea
Divyam <divyamgoyal878@gmail.com>
2023-11-09 18:39:54 +0530

    made add row popup

--- src/components/Expenses/FoodExpenses/FoodExpenses.js
+++ src/components/Expenses/FoodExpenses/FoodExpenses.js
@@ -1,141 +1,243 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import currencies from "../../currencies/currencies";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 function FoodExpenses() {
-  const [rows, setRows] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const [date, setDate] = useState('');
-  const [expenses, setExpenses] = useState('');
-  const [currency, setCurrency] = useState('');
-  const [amount, setAmount] = useState('');
-  const [bill, setBill] = useState('');
+	const [isModalOpen, setIsModalOpen] = useState(false);
+  const [expensesData, setExpensesData] = useState([]);
+  const [formData, setFormData] = useState({
+    date: "",
+    expenses: "",
+    currency: "",
+    amount: "",
+  });
 
-  const openModal = () => {
-    setIsOpen(true);
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setExpensesData([...expensesData, formData]);
+    setFormData({
+      date: "",
+      expenses: "",
+      currency: "",
+      amount: "",
+    });
   };
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
 
-  const handleSaveRow = () => {
-    const newRow = { date, expenses, currency, amount, bill };
-    setRows([...rows, newRow]);
-    setDate('');
-    setExpenses('');
-    setCurrency('');
-    setAmount('');
-    setBill('');
-  };
+	return (
+		<div>
+			<button
+				data-modal-toggle="authentication-modal"
+				className="add-button-purple"
+				type="button"
+				onClick={openModal}
+			>
+				Toggle modal
+			</button>
 
-  return (
-    <div>
-      {/* <h1 className="text-2xl font-semibold mb-4 text-center">Food Expenses</h1> */}
-
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md mb-4 mx-auto block add-button"
-        onClick={openModal}
+			{isModalOpen && (
+				<div
+        id="authentication-modal"
+        className={`fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full ${isModalOpen ? 'modal-reveal' : ''}`}
       >
-        Add Row
-      </button>
+					<div className="abc1">
+						<div className="container-expenses-outer">
+							{/* <div className="relative w-full max-w-md max-h-full"> */}
+							{/* <div className="relative bg-white rounded-lg shadow dark:bg-gray-700"> */}
+							<button
+								type="button"
+								data-modal-hide="authentication-modal"
+								className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								<svg
+									className="w-3 h-3"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 14 14"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+									/>
+								</svg>
+								<span className="sr-only">Close modal</span>
+							</button>
 
-      <div className="w-full overflow-x-auto">
-        <table className="min-w-full bg-white rounded-lg overflow-hidden">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 bg-blue-100 text-left text-xs font-medium text-gray-800 uppercase">
-                Date
-              </th>
-              <th className="px-6 py-3 bg-blue-100 text-left text-xs font-medium text-gray-800 uppercase">
-                Expenses
-              </th>
-              <th className="px-6 py-3 bg-blue-100 text-left text-xs font-medium text-gray-800 uppercase">
-                Currency
-              </th>
-              <th className="px-6 py-3 bg-blue-100 text-left text-xs font-medium text-gray-800 uppercase">
-                Amount
-              </th>
-              <th className="px-6 py-3 bg-blue-100 text-left text-xs font-medium text-gray-800 uppercase">
-                Bill
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows.map((row, index) => (
-              <tr key={index}>
-                <td className="px-6 py-4 border-b border-gray-200">{row.date}</td>
-                <td className="px-6 py-4 border-b border-gray-200">{row.expenses}</td>
-                <td className="px-6 py-4 border-b border-gray-200">{row.currency}</td>
-                <td className="px-6 py-4 border-b border-gray-200">{row.amount}</td>
-                <td className="px-6 py-4 border-b border-gray-200">{row.bill}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+							<div className="container-expenses ">
+								<h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+									Add Expense
+								</h3>
+								<form className="space-y-6" action="#">
+									<div className="flex flex-wrap -mx-2 mb-2">
+										<div className="w-full sm:w-1/2 px-2 mb-2">
+											<div className="flex flex-wrap -mx-2">
+												<div className="w-full sm:w-1/2 px-2 mb-2">
+													<label
+														htmlFor="date"
+														className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+													>
+														Date:
+													</label>
+													<br />
+													<input
+														type="date"
+														name="date"
+														id="date"
+														className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+														required
+													/>
+												</div>
+												<div className="w-full sm:w-1/2 px-2 mb-2">
+													<label
+														htmlFor="expenses"
+														className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+													>
+														Expenses:
+													</label>
+													<br />
+													<select
+														name="expenses"
+														id="expenses"
+														className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+													>
+														<option value="expense1">BREAKFAST</option>
+														<option value="expense2">LUNCH</option>
+														<option value="expense3">DINNER</option>
+														<option value="expense4">WATER BOTTLE</option>
+														<option value="expense5">TEA/COFFEE</option>
+													</select>
+												</div>
+											</div>
+										</div>
+										<div className="w-full sm:w-1/2 px-2 mb-2">
+											<div className="flex flex-wrap -mx-2">
+												<div className="w-full sm:w-1/2 px-2 mb-2">
+													<label
+														htmlFor="currency"
+														className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+													>
+														Currency:
+													</label>
+													<br />
+													<select
+														name="currency"
+														id="currency"
+														className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+													>
+														{currencies.map((currency) => (
+															<option key={currency.code} value={currency.code}>
+																{currency.name} ({currency.code})
+															</option>
+														))}
+													</select>
+												</div>
+												<div className="w-full sm:w-1/2 px-2 mb-2">
+													<label
+														htmlFor="amount"
+														className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+													>
+														Amount:
+													</label>
+													<br />
+													<input
+														type="number"
+														name="amount"
+														id="amount"
+														className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+														placeholder="Amount"
+														required
+													/>
+												</div>
+												<div className="w-full sm:w-1/2 px-2 mb-2">
+													<div>
+														<label
+															htmlFor="file"
+															className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+														>
+															Upload File:
+														</label>
+														<br />
+														<input
+															type="file"
+															name="file"
+															id="file"
+															className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+															accept=".pdf, .jpg, .png, .jpeg"
+														/>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className="w-full sm:w-1/2 px-2 mb-2">
+										<button
+											type="submit"
+											className="add-button-purple"
+										>
+											Add Expense
+										</button>
+										<button
+      data-modal-toggle="authentication-modal"
+      className="close-button"
+      type="button"
+      onClick={closeModal}
+    >
+      <FontAwesomeIcon icon={faTimes} />
+    </button>
 
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={closeModal}
-        contentLabel="Add Row"
-      >
-        <div className="abc">      
-      <div className="container-edit-outer">        
-        <div className="container-edit">
-        <h2 className="text-xl font-semibold mb-4">Add Row</h2>
-        <input
-          type="text"
-          placeholder="Date"
-          className="w-full border rounded p-2 mb-2"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Expenses"
-          className="w-full border rounded p-2 mb-2"
-          value={expenses}
-          onChange={(e) => setExpenses(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Currency"
-          className="w-full border rounded p-2 mb-2"
-          value={currency}
-          onChange={(e) => setCurrency(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Amount"
-          className="w-full border rounded p-2 mb-2"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Bill"
-          className="w-full border rounded p-2 mb-4"
-          value={bill}
-          onChange={(e) => setBill(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleSaveRow}
-        >
-          Save
-        </button>
-        <button
-          className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded ml-4"
-          onClick={closeModal}
-        >
-          Cancel
-        </button>
-        </div>
-        </div>
-        </div>
-      </Modal>
-    </div>
-  );
-}
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				// </div>
+				// </div>
+			)}
+		</div>
+    
+	)}
+
+ 
+<div className="tab-content">
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Expenses</th>
+        <th>Currency</th>
+        <th>Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      {expensesData.map((expense, index) => (
+        <tr key={index}>
+          <td>{expense.date}</td>
+          <td>{expense.expenses}</td>
+          <td>{expense.currency}</td>
+          <td>{expense.amount}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+
+
 
 export default FoodExpenses;
